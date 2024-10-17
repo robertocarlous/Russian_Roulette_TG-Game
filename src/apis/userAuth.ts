e5a17832330cf92544ef59678e75f6b3675937d1
Darasimi <topedarasimi5@gmail.com>
2024-10-17 23:57:12 +0100

    changes

--- src/apis/userAuth.ts
+++ src/apis/userAuth.ts
@@ -4,7 +4,7 @@ import { apiClient } from "./Controller";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const userAuth = async (data: any) => {
   try {
-    const response = await apiClient.post('/telegram-auth', data); // Change to actual endpoint
+    const response = await apiClient.post('/telegram-auth/', data); // Change to actual endpoint
     return response;
   } catch (error: any) {
     console.error("Error in userAuth:", error); // Log the error for debugging
